885329d1157ede43a27c81fb2c3cd873a79f7a4b
jenaalsup <63277580+jenaalsup@users.noreply.github.com>
2020-06-30 14:27:15 -0700

    Add articles listing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home' # <controller name>#<method name>
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index]
 end
