11551f2373d964ec47f6f8c364baed1e9e5adbe3
jenaalsup <63277580+jenaalsup@users.noreply.github.com>
2020-06-30 14:57:43 -0700

    Add contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home' # <controller name>#<method name>
-  get 'about', to: 'pages#about'
   resources :articles, only: [:show, :index]
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 end
