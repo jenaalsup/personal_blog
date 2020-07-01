4b16fc1b1212bba2df8d4b525c5effbca7b8e73e
jenaalsup <63277580+jenaalsup@users.noreply.github.com>
2020-07-01 00:31:48 -0700

    Add categories controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :articles, only: [:show, :index]
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  resources :categories, only: [:show, :index]
 end
