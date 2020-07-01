c85bdd073f645194b1fe71a3349703e4d564001c
jenaalsup <63277580+jenaalsup@users.noreply.github.com>
2020-07-01 00:20:39 -0700

    Add category model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_30_190515) do
+ActiveRecord::Schema.define(version: 2020_07_01_071501) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
   end
 
 end
