f1c2d8dca481278dd04595d70d2a3da21fb44c08
jenaalsup <63277580+jenaalsup@users.noreply.github.com>
2020-07-01 00:25:06 -0700

    Add validations to category model

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,9 +2,17 @@ require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
 
-  test "category should be valid" do
+  def setup
     @category = Category.new(name: "Veganism")
+  end
+
+  test "category should be valid" do
     assert @category.valid?
+  end
+
+  test "name should be present" do
+    @category.name = " "
+    assert_not @category.valid?
   end
 
 end
