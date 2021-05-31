c5233bc1472702096f2b6c9e63f59f083ead7472
jenaalsup <63277580+jenaalsup@users.noreply.github.com>
2021-05-30 23:36:36 -0700

    Add contribute page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home' # <controller name>#<method name>
   resources :articles, only: [:show, :index]
+  get 'contribute', to: 'pages#contribute'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   resources :categories, only: [:show, :index]
